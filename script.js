f38f5c2144fc9dc746fc1eb667092d7c608087b4
Zhihao Yang <yang_zhihao@live.com>
2016-03-15 14:37:54 +0100

    bug fix

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function paintNetwork(newNodes){
 
     var newlinks = nodes.map(function(d){
       var targetIndex = nodes.map(function(n){return n.name}).indexOf(d.name);
-      linkedByIndex["0," + targetIndex] = true;
+      linkedByIndex[sourceIndex + "," + targetIndex] = true;
       return {"source": sourceIndex, "target": targetIndex, "weight": 1}
     });
     for(key in newlinks){
@@ -118,7 +118,7 @@ function paintNetwork(newNodes){
     }
     var newlinks = newNodes.map(function(d, i){
       var targetIndex = nodeLength + i;
-      linkedByIndex[source + "," + targetIndex] = true;
+      linkedByIndex[sourceIndex + "," + targetIndex] = true;
       return {"source": sourceIndex, "target": targetIndex, "weight": 1 }
     });
 
@@ -184,12 +184,18 @@ function paintNetwork(newNodes){
     })
     .style("stroke-width", function(o) { 
       var weight = Math.sqrt(d.weight);
-      return o.source.index == d.index || o.target.index == d.index ? weight*2 : weight;
+      console.log("weight: "+weight);
+      return o.source.index == d.index || o.target.index == d.index ? weight*2 : 0.5;
     });
 
     var c = svg.selectAll('circle');
     c.style("stroke", function(o) {
       return isConnected(d, o) ? "red" : "white";
+    })
+    .style("stroke-width", function(o) { 
+      var weight = Math.sqrt(d.weight);
+      console.log("weight: "+weight);
+      return isConnected(d, o) ? weight*2 : 0.5;
     });
   }
 
@@ -204,7 +210,8 @@ function paintNetwork(newNodes){
     .style("stroke-width", function(o) { return Math.sqrt(o.weight); })
 
     var c = svg.selectAll('circle');
-    c.style("stroke", "black");
+    c.style("stroke", "black")
+    .style("stroke-width", 0.5);
   }
 
   // re-fetch data
