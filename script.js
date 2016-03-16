d4708cedee7e4a36b8fb362e32dbe7e934ca03e9
daisydu <857635228@qq.com>
2016-03-16 17:23:55 +0100

    Chg the node color when it is parent

--- script.js
+++ script.js
@@ -230,9 +230,10 @@ function paintNetwork(newNodes){
 
   node.append("circle")
       .attr("class", function(d){return d.source === true ? "source" : null})
+      .attr("fill", function(d){
+        return sourceIndexArray.indexOf(d.index) != -1 ? "#DE6F06" : "#FF9814";
+      })
       .attr("r",function(d){
-        console.log("circle");
-        console.log(sourceIndexArray.indexOf(d.index));
         return sourceIndexArray.indexOf(d.index) != -1 ? 15 : 8;
       });
 
